53e1be933d1ebc8d5b06490d045f9ff41d212370
Maxks <maxksimronn@gmail.com>
2021-07-02 01:06:37 +0300

    Added Header component and logo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <Link className={styles.logoContainer} to="/">
+        <Logo />
+      </Link>
+      <div className={styles.options}>
+        <Link to="/shop">SHOP</Link>
+        <Link to="/contact">CONTACT US</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
